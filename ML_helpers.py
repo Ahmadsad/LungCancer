86c053c5888d2b674a7676b77da0ae04f8a85601
Ahmad Sadek <a7madsadeq@gmail.com>
2022-03-16 17:55:06 +0100

    all prep and cleaning done on linguistic data

---
+++ ML_helpers.py
@@ -0,0 +1,50 @@
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix
+
+from sklearn.metrics import classification_report
+from lime import lime_tabular
+
+import shap
+import numpy as np
+import random
+
+def get_AUC_ROC_value(X_test, Y_test, model):
+    pred = np.array(model.predict(X_test))
+    true = np.array(Y_test)
+    auc = roc_auc_score(true, pred)
+    print("AUC:", auc)
+    return auc
+
+def get_train_test_split_data(data_list, labels, test_size=0.33, random_state=50):
+    X_train, X_test, Y_train, Y_test = train_test_split(data_list, labels, 
+                                                  test_size=test_size, random_state=random_state)
+    return (X_train, X_test, Y_train, Y_test)
+
+def get_tfidf_vectorization_model(data_list):
+    return TfidfVectorizer().fit(data_list)
+
+def get_tfidf_vectorized_data(to_be_vectorized_data, tfidf_model=None, tfidfTrain_data_list=None):
+    if tfidf_model is None:
+        vect = TfidfVectorizer().fit(tfidfTrain_data_list)
+    else:
+        vect = tfidf_model
+    return vect.transform(to_be_vectorized_data)
+
+def get_csr_matrix(vectorized_data_list, dtype="float"):
+    """
+    efficient arithmetic operations CSR + CSR, CSR * CSR, etc.
+    efficient row slicing
+    fast matrix vector products
+    """
+    return csr_matrix(vectorized_data_list, dtype=dtype).toarray()
+
+def get_classification_report(cls, test_data, test_labels, target_names=["cancer","no cancer"]):
+    return classification_report(test_labels, cls.predict(test_data), target_names=target_names)
+
+def get_shap_kernel_explainer_and_values(cls, training_data, test_data):
+    explainer = shap.KernelExplainer(cls.predict_proba, training_data)
+    shap_values = explainer.shap_values(test_data)
+    return (explainer, shap_values)
+
--- helpers_and_variables.py
+++ helpers_and_variables.py
@@ -1,6 +1,7 @@
 from nltk.corpus import stopwords
 import pandas as pd
 from nltk.stem import SnowballStemmer
+from sklearn.metrics import roc_auc_score
 
 import win32com.client as win32
 import string
@@ -71,20 +72,14 @@ def _r1_scandinavian(word, vowels):
 def stem(word):
     """
     Stem a Swedish word and return the stemmed form.
-
     :param word: The word that is stemmed.
     :type word: str or unicode
     :return: The stemmed form.
     :rtype: unicode
-
     """
     word = word.lower()
-    
-#     print(stopwords)
-    
     if word in stopwords: 
         return word
-    
     r1 = _r1_scandinavian(word, __vowels)
 
     # STEP 1
@@ -117,14 +112,13 @@ def stem(word):
                 word = word[:-1]
             break
             
-# remove question numbers like br_1, br2...
+    # remove question numbers like br_1, br2...
     for suffix in __step4_suffix:
         if suffix in word:
             indx = word.index(suffix)
             if word == suffix or has_numbers(word):
                 word = ""
                 break;
-        
     return word
 
 def has_numbers(inputString): 
@@ -141,10 +135,96 @@ def get_exL_df(stringPath, password=None, sheetNum=0):
         xlwb = xlApp.Workbooks.Open(stringPath, False, True, None, password)
     else:
         xlwb = xlApp.Workbooks.Open(stringPath, False, True, None)
-    return pd.DataFrame(xlwb.Sheets(sheetNum).UsedRange())
+    rowDataFrame = pd.DataFrame(xlwb.Sheets(sheetNum).UsedRange())
+    rowDataFrame.columns = rowDataFrame.iloc[0,:]
+    return rowDataFrame
+
+def get_cleanedCol_rowData_df(rawDataFrame, columns_toBe_removed=None):
+    if columns_toBe_removed is None:
+        columns_toBe_removed = ["Diagnos2", "Othercancer","Mutation_FULL", "Stage_gr", "DiagnosticInvestigation","PADdatum",
+#                 "STUDY_1","InterviewDate", "Death_date", "DEATH_date_final", "Date_Background", "Intdate_Background",
+                                "Lungcancer_Num", "ALL_HISTOLOGIES_ScLC_NScLC_NE", "ALL_HISTOLOGIES", 
+                                "ALL_HISTOLOGIES_CompleteOtherCancer", "Other_cancer", "Metastases", 
+                                "NoCancer_AdvancedStage_Ordinal", "LC_LCNEC_ejLC_ALL_2017control", "Sensitivity_LC_LCNEC_MM_ejLC", 
+                                "BAKGRUND", "BREATHE", "COUGH", "PHLEGM", "PAIN_ACHES_DISCOMFORT", "FATIGUE", "VOICE",
+                                "APPETITE_TASTE_EATING", "SMELL", "FEVER", "OTHER_CHANGES", "CURRENT_HEALTH_EORTC"]
+        try:
+            rawDataFrame = rawDataFrame.drop(labels=columns_toBe_removed, axis=1, inplace=False)
+        except:
+            rawDataFrame.columns = rawDataFrame.iloc[0,:]
+            rawDataFrame = rawDataFrame.drop(labels = columns_toBe_removed, axis=1, inplace=False)
+        return rawDataFrame
+
+def get_dates_in_days(rawDataFrame, referens_date_col=None):
+    if referens_date_col is None:
+        referens_date_col = list(rawDataFrame["InterviewDate"])
+    else:
+        referens_date_col = list(rawDataFrame[referens_date_col])
+
+    date_cols = get_cols_with_dates(rawDataFrame)
+    # put the referense last, to be subtracted lastly
+    if referens_date_col[0] in date_cols:
+        date_cols.remove(referens_date_col[0])
+        date_cols.insert(len(date_cols), referens_date_col[0])
+
+    # get corretly formed date list as reference
+    referens_dates_list = get_converted_toCorrect_form_datesList(referens_date_col);
+
+    for date_col in date_cols:
+        
+        if date_col == "PADdatum":
+            current_col = list(rawDataFrame['PADdatum'][1:].astype('str'))
+            for idx in range(0, len(current_col)):
+                tmp = ""
+                for x in current_col[idx].split():
+                    if x.isdigit():
+                        tmp = tmp + str(x)
+                current_col[idx] = tmp    
+            rawDataFrame[date_col][1:] = current_col
+            
+        # get a specific date column in correct form and as list
+        dates_list = get_converted_toCorrect_form_datesList(list(rawDataFrame[date_col]));
+
+        for idx in range(1,len(rawDataFrame[date_col])):
+            if dates_list[idx] != '#NULL!':
+                try:
+                    dates_list[idx] =  referens_dates_list[idx] - dates_list[idx];
+
+                except:
+                    dates_list[idx] = dates_list[idx]
+
+        rawDataFrame[date_col] = dates_list          
+    return rawDataFrame
+    
+def get_cols_with_dates(rawDataFrame, num_cols=None):
+    # other columns with dates but not mentioned in the column name or description
+    date_cols = ["Breathe_46", "V3", "V3_Ph_1", "V3_Pain", "V3_Fa_1", "V3_Vo", "V3_Sm", "Fever_2", "Fever_5", "Fever_8",
+                        "Fever_11", "Fever_14", "Fever_17", "Fever_20", "Fever_23", "Fever_24", "Otherchanges_2", "Otherchanges_5", 
+                        "Otherchanges_8", "Otherchanges_11", "Otherchanges_14", "Otherchanges_17", "Otherchanges_20", "Otherchanges_23",
+                        "Otherchanges_26", "Otherchanges_30", "Otherchanges_34", "Otherchanges_38"]
+    for col in rawDataFrame.columns:
+        if ('date' in col.lower() or 'datum' in col.lower()):
+            date_cols.append(col)    
+    if num_cols is None:
+        return date_cols
+    else:
+        return date_cols[0:num_cols]
+    
+def get_converted_toCorrect_form_datesList(dates_list):
+    dates_list = list(dates_list)
+    for idx in range(1,len(dates_list)):
+        if dates_list[idx] != "#NULL!":
+            try:
+                dates_list[idx] = pd.to_datetime(dates_list[idx], format='%y%m%d', errors='ignore');
+                dates_list[idx] = pd.to_datetime(dates_list[idx], errors='ignore');
+            except:
+                dates_list[idx] = dates_list[idx]
+    return dates_list
+    
 
 def get_cleaned_dataInfo_df(stringPath):
     dataInfoDF = get_exL_df(stringPath = stringPath, sheetNum = 1)
+    dataInfoDF.columns = range(0,len(dataInfoDF.columns))
     dataInfoDF.drop(columns=[1,3], inplace=True)
     dataInfoDF.rename(columns={0: dataInfoDF.iloc[1,0], 2:dataInfoDF.iloc[1,1]}, inplace=True)
     dataInfoDF.drop([0,1,713], axis=0, inplace=True)
@@ -153,12 +233,27 @@ def get_cleaned_dataInfo_df(stringPath):
     
 def get_cleaned_katInfo_df(stringPath):
     katInfoDF = get_exL_df(stringPath = stringPath, sheetNum = 3)
+    katInfoDF.columns = range(0,len(katInfoDF.columns))
     katInfoDF.drop(columns=[3], inplace=True)
     katInfoDF.rename(columns={0: "Variable", 1:"Label", 2:"Value"}, inplace=True)
     katInfoDF.drop([0,1], axis=0, inplace=True)
     return katInfoDF
 
-    
+def get_labels_and_indicesOfUnlabeledPatients(rowDataFrame):
+    labels = list(rowDataFrame['Lungcancer_Num'])[1:]
+    label_list = list()
+    removed_indices = list()
+    for ind in range(0, len(labels)):
+        value = labels[ind]
+        if value == 1.0 or value == 2.0:
+            label_list.append(value)
+        else:
+            removed_indices.append(ind+1)
+            
+    rowDataFrame = rowDataFrame.drop(labels=removed_indices, axis=0, inplace=False)
+
+    return rowDataFrame, label_list, removed_indices
+
 def get_dict_of_dataInfoDF(dataFrame):
     _dict = dict()
     list_of_questions = dataFrame['Label'].tolist()
@@ -201,8 +296,6 @@ def get_cleaned_list_of_strings(listOfStrings, stemm = False, stemm_by_nltk=Fals
     for text in listOfStrings:
         if text is None: 
             text = 'No/missing'
-#         if type(text) is not list:
-#             text = list(text)
         text = text.replace('/',' ').replace('…','').replace('”','').replace('_',' ')
         words = text.lower().translate(str.maketrans('', '', punctations))
         words = remove_digits_at_start(words)
@@ -256,38 +349,101 @@ def get_tokenized_strings_by_nltk(listOfStrings):
 
     return output_words_after_tokenize
     
-def get_dict_of_questions_answers(row_DF, dataInfo_DF, katInfoDF, amount_data=None):
+def get_dict_of_questions_answers(raw_DF, dataInfoDF, katInfoDF, amount_data=None, clear_missings_or_Non=False):
     main_dict = dict()
     tmp_patient_dict = dict()
-    list_of_questions = dataInfo_DF['Label'].tolist()
-    list_of_quesNams = dataInfo_DF['Variable'].tolist()
+   
     dict_of_katInfo = get_dict_of_katInfoDF(katInfoDF)
+    dict_of_dataInfo = get_dict_of_dataInfoDF(dataInfoDF)
     
-    if amount_data is None or amount_data > len(row_DF):
-        n=len(row_DF)
+    missing_answer = False   
+    list_count_removed_features = list()
+    
+    if amount_data is None or amount_data > len(raw_DF):
+        n = len(raw_DF)
     else:
         n = amount_data    
-        
-    for row in range(1, len(row_DF)):
+    # dont need patient number as feature
+    features = list(raw_DF.columns)[1:]
+    for patient_ind in range(1, n):
         ind_ques = 0
-        for ind_ques in range(0, len(row_DF.iloc[row,:])):
-            item = row_DF.iloc[row,ind_ques]
-            value = list_of_questions[ind_ques]
-            if remove_tags(list_of_questions[ind_ques]) is None or remove_tags(list_of_questions[ind_ques]) is "":
+        count_removed_features = 0 
+        
+        ind_ques = 1
+        for feature in features:   #range(1, len(raw_DF.iloc[patient_ind,:])):
+            # the answer of the patient
+            answer = raw_DF.iloc[patient_ind][feature]
+            # item = raw_DF.iloc[patient_ind,ind_ques]
+            # the question's text instead of only numbers or question name
+            value =  dict_of_dataInfo[feature] #  list_of_questions[ind_ques]
+            # Remove tags, such as html tags
+            if remove_tags(value) is None or remove_tags(value) is "":
                 value = ""
-    #         if type(item) is float: item = int(item)
-            if row_DF.iloc[0, ind_ques] in dict_of_katInfo:
-                ques_dict = dict_of_katInfo[row_DF.iloc[0, ind_ques]]
-                if str(int(item)) in ques_dict:
-                    item = ques_dict[str(int(item))]
-                else: # missing answer
-                    item = "No/missing"
-            if item == '#NULL' or item == '#N/A'or item == '#N/A!' or item == '#NULL!' or item == 'None' or item is None:
-                item = "No/missing"
-
-            value = value + ": " + str(item)
-            tmp_patient_dict[list_of_quesNams[ind_ques]] = value
-
-        main_dict[str(int(row_DF.iloc[row,0]))] = tmp_patient_dict
+            # check if we have categorical info for this specific column name [0, ind_ques], 
+            # and replace numbers with corresponding text
+            if feature in dict_of_katInfo:
+                ques_dict = dict_of_katInfo[feature]
+                if str(int(answer)) in ques_dict:
+                    # Remove tags, such as html tags and assign the correct text to the corres. number
+                    answer = remove_tags(ques_dict[str(int(answer))])
+                else: # if missing answer replace with NO/missing
+                    answer = "No/missing"
+            if (
+                answer == '#N/A'or answer == '#N/A!' or 
+                answer == '#NULL' or  answer == '#NULL!' or 
+                answer == 'None' or answer is None or
+                answer == "" or answer == "None/missing" or 
+                answer == "No/missing" or answer == -2146826288 or
+                "missing" in str(answer) or "NaT" in str(answer)
+            ):
+                answer = "No/missing"
+                missing_answer = True
+            # clear questions with no answers, which includes modules such as, Breath, Cough (empty answers, used to seperat modules)
+            if clear_missings_or_Non and missing_answer:
+                missing_answer = False
+                count_removed_features = count_removed_features + 1
+                continue
+            value = value + ": " + str(answer)
+            tmp_patient_dict[feature] = value
+            ind_ques = ind_ques + 1
+        main_dict[str(int(raw_DF.iloc[patient_ind,0]))] = tmp_patient_dict
         tmp_patient_dict = dict()
-    return main_dict
+        
+        # add 2 for the labels at first rows and since pandas aounts from 1 already
+        list_count_removed_features.append((patient_ind+2, count_removed_features))
+        
+    return main_dict, list_count_removed_features
+
+def get_data_list_from_main_dict(main_dict, stemm=True):
+    data_list = list()
+    for key in main_dict.keys():
+        patient_text = list(main_dict[key].values())
+        patient_cleaned_text = get_cleaned_list_of_strings(patient_text, stemm=stemm)
+        tmp_patient_text = ' '.join(patient_cleaned_text)
+        data_list.append(tmp_patient_text)
+    return data_list
+        
+def write_dict_as_json_file(dict_toBe_saved, file_path = None):
+    import json
+    if file_path is None:
+        file_path = "C:/Users/a7mad/Desktop/MEX/PekLung/dict.json"
+    # create json object from dictionary
+    obj = json.dumps(dict_toBe_saved)
+    # open file for writing, "w" 
+    f = open(file_path,"w")
+    # write json object to file
+    f.write(obj)
+    # close file
+    f.close()
+    
+def read_dict_from_json_file(file_path=None):
+    import json
+    if file_path is None:
+        file_path = "C:/Users/a7mad/Desktop/MEX/PekLung/dict.json"
+    # reading the data from the file
+    with open(file_path) as f:
+        data = f.read()
+    # reconstructing the data as a dictionary
+    js = json.loads(data)
+    return js
+
